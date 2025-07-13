51a3cbe6831c50ab0a85a0b39bb6a1b7c5aea52d
Yokohama <artuzcapati2@gmail.com>
2025-07-13 11:26:05 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# what is this
+portfolio website  
+wip
---
+++ script.js
@@ -0,0 +1,5 @@
+let body = document.body;
+let div = document.createElement("div");
+div.innerHTML = "Jackdaws love my big sphinx of quartz.";
+div.style.color = "tomato";
+body.appendChild(div);

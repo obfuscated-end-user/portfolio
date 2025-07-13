ddd70b2e83b0f30437ffa605e1f9849c346e8747
Yokohama <artuzcapati2@gmail.com>
2025-07-13 11:32:17 +0800

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let body = document.body;
-let div = document.createElement("div");
-div.innerHTML = "Jackdaws love my big sphinx of quartz.";
-div.style.color = "tomato";
-body.appendChild(div);
+
+let div1 = document.createElement("div");
+div1.innerHTML = "Jackdaws love my big sphinx of quartz.";
+div1.style.color = "tomato";
+
+let div2 = document.createElement("div");
+div2.innerHTML = "bottom text";
+div2.style.color = "mediumaquamarine";
+
+body.appendChild(div1);
+body.appendChild(div2);
